f30512ce67263fb7d2744c683cfe0d8806e5bf4a
Omkar Gunjal <omkarbg007@gmail.com>
2026-01-11 11:18:11 +0530

    update domain

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,21 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// --- PASTE YOUR KEYS INSIDE THIS BLOCK ---
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBDuxUxmldUoef45N-3mG_hdMauFgMNSq4",
-  authDomain: "jee-planner-97951.firebaseapp.com",
-  projectId: "jee-planner-97951",
-  storageBucket: "jee-planner-97951.firebasestorage.app",
-  messagingSenderId: "975684471638",
-  appId: "1:975684471638:web:9a2f031d261b0f784024f7",
-  measurementId: "G-Q1840GE3PW"
+  apiKey: "AIzaSyC310A6fhBb04sOmUjV_vFoZsOxDpUhc7E",
+  authDomain: "preppilotapp.firebaseapp.com",
+  projectId: "preppilotapp",
+  storageBucket: "preppilotapp.firebasestorage.app",
+  messagingSenderId: "391244178642",
+  appId: "1:391244178642:web:948b4e19b3b8b5a292f90b",
+  measurementId: "G-NHW7F78504"
 };
 
-// --- THIS IS THE PART THAT WAS MISSING ---
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// We need to 'export' these so App.js can use them:
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
